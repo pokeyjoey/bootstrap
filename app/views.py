5bdffec1cf2a25a4dafdd246ba6caaaaab24f113
Jeff Roche <jkeauctions@gmail.com>
2015-01-29 07:17:43 -0800

    Removed cruft from microblog flask tutorial.

--- app/views.py
+++ app/views.py
@@ -1,26 +1,6 @@
 from flask import render_template
 from app import app
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': 'Miguel'}
-
-    posts = [
-        {
-            'author':{'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author':{'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', 
-        title='Home', 
-        user=user,
-        posts=posts)
-
 @app.route('/grid')
 def grid():
     return render_template('grid.html', title='Grid')
